c3e120bacba594cda145073269d37d16a6f5197a
YARE0909 <pradyumnad092004@gmail.com>
2025-07-03 12:58:52 +0530

    Changes

--- src/app/exam/[id]/page.tsx
+++ src/app/exam/[id]/page.tsx
@@ -802,12 +802,12 @@ export default function ExamPage() {
                         </div>
                       </div>
                     </div>
-                    <div className="flex flex-col md:flex md:flex-row items-center justify-between bg-gray-100 p-4">
+                    <div className="flex flex-col md:flex md:flex-row items-center justify-between bg-gray-100 px-4 py-2">
                       <div className="w-full flex gap-3">
                         <button
                           onClick={toggleMarkForReview}
                           className={clsx(
-                            "w-full md:w-fit px-4 py-1 rounded-md font-medium cursor-pointer bg-white hover:bg-gray-200 border border-gray-300"
+                            "w-full md:w-fit px-4 py-1 rounded-md font-medium cursor-pointer bg-white hover:bg-gray-200 border border-gray-300 text-sm"
                           )}
                         >
                           Mark For Review & Next
@@ -821,7 +821,7 @@ export default function ExamPage() {
                         <button
                           onClick={clearResponse}
                           className={clsx(
-                            "w-full md:w-fit px-4 py-1 rounded-md font-medium cursor-pointer bg-white hover:bg-gray-200 border border-gray-300"
+                            "w-full md:w-fit px-4 py-1 rounded-md font-medium cursor-pointer bg-white hover:bg-gray-200 border border-gray-300 text-sm"
                           )}
                         >
                           Clear Response
@@ -844,7 +844,7 @@ export default function ExamPage() {
                           <div className="w-full md:w-fit">
                             <button
                               onClick={handleSubmit}
-                              className="w-full text-nowrap px-6 py-1 bg-cyan-600 hover:bg-cyan-700 text-white rounded-md font-medium cursor-pointer mb-16 md:mb-0"
+                              className="w-full text-nowrap px-6 py-1 bg-cyan-600 hover:bg-cyan-700 text-white rounded-md font-medium cursor-pointer mb-16 md:mb-0 text-sm"
                             >
                               Submit Test
                             </button>
@@ -857,7 +857,7 @@ export default function ExamPage() {
                                 currentIndex + 1 === questionsMeta.length
                               }
                               className={clsx(
-                                "w-full md:w-fit px-6 py-1 rounded-md font-medium text-white transition cursor-pointer bg-blue-600 hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500 whitespace-nowrap"
+                                "w-full md:w-fit px-6 py-1 rounded-md font-medium text-white transition cursor-pointer bg-blue-600 hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500 whitespace-nowrap text-sm"
                               )}
                             >
                               Save & Next
