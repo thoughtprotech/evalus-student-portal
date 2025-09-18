87378798c0d367106c050974741e63348515ae55
thoughtprotech <thoughtprotechnologies@gmail.com>
2025-09-18 19:33:07 +0530

    Layout added

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -72,174 +72,177 @@ export default function ProfilePage() {
   }
 
   return (
-    <div className="w-full h-full flex flex-col items-center gap-6">
-      <div className="w-full max-w-4xl flex flex-col lg:flex lg:flex-row justify-between lg:items-center gap-4">
-        <div className=" flex items-center gap-8">
-          {candidate && (
-            <EditableImage
-              firstName={candidate.FirstName}
-              lastName={candidate.LastName}
-              src={candidate.IUserPhoto}
-              onEdit={handleImageUpdate}
-            />
-          )}
-          <div className="flex flex-col">
-            <div className="flex flex-col">
-              {candidate && (
-                <EditableText
-                  text={candidate.LastName}
-                  onSubmit={(text) => handleUserUpdate(text, "LastName")}
-                  className="text-xl font-bold text-indigo-600"
-                  inputClassName="p-2 border border-gray-300 rounded-md"
-                />
-              )}
-              {candidate && (
-                <EditableText
-                  text={candidate.FirstName}
-                  onSubmit={(text) => handleUserUpdate(text, "FirstName")}
-                  className="text-5xl font-bold text-gray-800"
-                  inputClassName="text-5xl p-2 border border-gray-300 rounded-md"
-                />
-              )}
-            </div>
-            <div className="text-xl font-medium text-gray-600">
-              {candidate && (
-                <EditableText
-                  text={candidate.DisplayName}
-                  onSubmit={(text) => handleUserUpdate(text, "DisplayName")}
-                  className="text-xl font-bold text-gray-800"
-                  inputClassName="w-fit p-2 border border-gray-300 rounded-md"
-                />
-              )}
-            </div>
-          </div>
-        </div>
-        <div>
-          <UpdatePassword handleUserUpdate={handleUserUpdate} />
-        </div>
+    <div className="w-full h-full flex flex-col items-center bg-gray-50 min-h-screen">
+      <div className="w-full bg-white shadow-md py-6 mb-8">
+        <h1 className="text-3xl font-bold text-center text-indigo-700">Profile Information</h1>
       </div>
-
-      <div className="w-full max-w-4xl flex flex-col gap-6">
-        {/* Account Information Section */}
-        <div className="bg-white p-4 rounded-lg shadow-md flex flex-col gap-4">
-          <div className="flex items-center gap-2">
-            <User className="text-indigo-500" />
-            <h2 className="font-semibold text-lg text-gray-800">Account Information</h2>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="w-full max-w-4xl flex flex-col items-center gap-6">
+        <div className="w-full flex flex-col lg:flex lg:flex-row justify-between lg:items-center gap-4">
+          <div className="flex items-center gap-8">
             {candidate && (
-              <>
-                <div>
-                  <h3 className="font-medium text-gray-600">Display Name</h3>
+              <EditableImage
+                firstName={candidate.FirstName}
+                lastName={candidate.LastName}
+                src={candidate.IUserPhoto}
+                onEdit={handleImageUpdate}
+              />
+            )}
+            <div className="flex flex-col">
+              <div className="flex flex-col">
+                {candidate && (
+                  <EditableText
+                    text={candidate.LastName}
+                    onSubmit={(text) => handleUserUpdate(text, "LastName")}
+                    className="text-xl font-bold text-indigo-600"
+                    inputClassName="p-2 border border-gray-300 rounded-md"
+                  />
+                )}
+                {candidate && (
+                  <EditableText
+                    text={candidate.FirstName}
+                    onSubmit={(text) => handleUserUpdate(text, "FirstName")}
+                    className="text-5xl font-bold text-gray-800"
+                    inputClassName="text-5xl p-2 border border-gray-300 rounded-md"
+                  />
+                )}
+              </div>
+              <div className="text-xl font-medium text-gray-600">
+                {candidate && (
                   <EditableText
                     text={candidate.DisplayName}
                     onSubmit={(text) => handleUserUpdate(text, "DisplayName")}
-                    className="text-lg text-gray-800"
-                    inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                    className="text-xl font-bold text-gray-800"
+                    inputClassName="w-fit p-2 border border-gray-300 rounded-md"
                   />
-                </div>
-                <div>
-                  <h3 className="font-medium text-gray-600">User Name</h3>
-                  <div className="text-lg text-gray-800 p-2">{candidate.Email}</div>
-                </div>
-              </>
-            )}
+                )}
+              </div>
+            </div>
+          </div>
+          <div>
+            <UpdatePassword handleUserUpdate={handleUserUpdate} />
           </div>
         </div>
-        {/* Contact Information Section */}
-        <div className="bg-white p-4 rounded-lg shadow-md flex flex-col gap-4">
-          <div className="flex items-center gap-2">
-            <Mail className="text-indigo-500" />
-            <h2 className="font-semibold text-lg text-gray-800">Contact Information</h2>
+
+        <div className="w-full flex flex-col gap-6">
+          {/* Account Information Section */}
+          <div className="bg-white p-4 rounded-lg shadow-md flex flex-col gap-4">
+            <div className="flex items-center gap-2">
+              <User className="text-indigo-500" />
+              <h2 className="font-semibold text-lg text-gray-800">Account Information</h2>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {candidate && (
+                <>
+                  <div>
+                    <h3 className="font-medium text-gray-600">Display Name</h3>
+                    <EditableText
+                      text={candidate.DisplayName}
+                      onSubmit={(text) => handleUserUpdate(text, "DisplayName")}
+                      className="text-lg text-gray-800"
+                      inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-medium text-gray-600">User Name</h3>
+                    <div className="text-lg text-gray-800 p-2">{candidate.Email}</div>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* Contact Information Section */}
+          <div className="bg-white p-4 rounded-lg shadow-md flex flex-col gap-4">
+            <div className="flex items-center gap-2">
+              <Mail className="text-indigo-500" />
+              <h2 className="font-semibold text-lg text-gray-800">Contact Information</h2>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {candidate && (
+                <>
+                  <div>
+                    <h3 className="font-medium text-gray-600">Email</h3>
+                    <EditableText
+                      text={candidate.Email}
+                      onSubmit={(text) => handleUserUpdate(text, "Email")}
+                      className="text-lg text-gray-800"
+                      inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                      type="email"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-medium text-gray-600">Phone Number</h3>
+                    <EditableText
+                      text={candidate.PhoneNumber}
+                      onSubmit={(text) => handleUserUpdate(text, "PhoneNumber")}
+                      className="text-lg text-gray-800"
+                      inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                      type="phone"
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="bg-white p-4 rounded-lg shadow-md flex flex-col gap-4">
+            <div className="flex items-center gap-2">
+              <MapPin className="text-red-500" />
+              <h2 className="font-semibold text-lg text-gray-800">Address</h2>
+            </div>
             {candidate && (
               <>
-                <div>
-                  <h3 className="font-medium text-gray-600">Email</h3>
-                  <EditableText
-                    text={candidate.Email}
-                    onSubmit={(text) => handleUserUpdate(text, "Email")}
-                    className="text-lg text-gray-800"
-                    inputClassName="w-full p-2 border border-gray-300 rounded-md"
-                    type="email"
-                  />
-                </div>
-                <div>
-                  <h3 className="font-medium text-gray-600">Phone Number</h3>
-                  <EditableText
-                    text={candidate.PhoneNumber}
-                    onSubmit={(text) => handleUserUpdate(text, "PhoneNumber")}
-                    className="text-lg text-gray-800"
-                    inputClassName="w-full p-2 border border-gray-300 rounded-md"
-                    type="phone"
-                  />
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <div>
+                    <h3 className="font-medium text-gray-600">Street Address</h3>
+                    <EditableText
+                      text={candidate.Address}
+                      onSubmit={(text) => handleUserUpdate(text, "Address")}
+                      className="text-lg text-gray-800"
+                      inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-medium text-gray-600">City</h3>
+                    <EditableText
+                      text={candidate.City}
+                      onSubmit={(text) => handleUserUpdate(text, "City")}
+                      className="text-lg text-gray-800"
+                      inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-medium text-gray-600">State</h3>
+                    <EditableText
+                      text={candidate.State}
+                      onSubmit={(text) => handleUserUpdate(text, "State")}
+                      className="text-lg text-gray-800"
+                      inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-medium text-gray-600">Postal Code</h3>
+                    <EditableText
+                      text={candidate.PostalCode}
+                      onSubmit={(text) => handleUserUpdate(text, "PostalCode")}
+                      className="text-lg text-gray-800"
+                      inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                      type="number"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-medium text-gray-600">Country</h3>
+                    <EditableText
+                      text={candidate.Country}
+                      onSubmit={(text) => handleUserUpdate(text, "Country")}
+                      className="text-lg text-gray-800"
+                      inputClassName="w-full p-2 border border-gray-300 rounded-md"
+                    />
+                  </div>
                 </div>
               </>
             )}
           </div>
+          {/* Notes section removed */}
         </div>
-
-        <div className="bg-white p-4 rounded-lg shadow-md flex flex-col gap-4">
-          <div className="flex items-center gap-2">
-            <MapPin className="text-red-500" />
-            <h2 className="font-semibold text-lg text-gray-800">Address</h2>
-          </div>
-          {candidate && (
-            <>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div>
-                  <h3 className="font-medium text-gray-600">Street Address</h3>
-                  <EditableText
-                    text={candidate.Address}
-                    onSubmit={(text) => handleUserUpdate(text, "Address")}
-                    className="text-lg text-gray-800"
-                    inputClassName="w-full p-2 border border-gray-300 rounded-md"
-                  />
-                </div>
-                <div>
-                  <h3 className="font-medium text-gray-600">City</h3>
-                  <EditableText
-                    text={candidate.City}
-                    onSubmit={(text) => handleUserUpdate(text, "City")}
-                    className="text-lg text-gray-800"
-                    inputClassName="w-full p-2 border border-gray-300 rounded-md"
-                  />
-                </div>
-                <div>
-                  <h3 className="font-medium text-gray-600">State</h3>
-                  <EditableText
-                    text={candidate.State}
-                    onSubmit={(text) => handleUserUpdate(text, "State")}
-                    className="text-lg text-gray-800"
-                    inputClassName="w-full p-2 border border-gray-300 rounded-md"
-                  />
-                </div>
-                <div>
-                  <h3 className="font-medium text-gray-600">Postal Code</h3>
-                  <EditableText
-                    text={candidate.PostalCode}
-                    onSubmit={(text) => handleUserUpdate(text, "PostalCode")}
-                    className="text-lg text-gray-800"
-                    inputClassName="w-full p-2 border border-gray-300 rounded-md"
-                    type="number"
-                  />
-                </div>
-                <div>
-                  <h3 className="font-medium text-gray-600">Country</h3>
-                  <EditableText
-                    text={candidate.Country}
-                    onSubmit={(text) => handleUserUpdate(text, "Country")}
-                    className="text-lg text-gray-800"
-                    inputClassName="w-full p-2 border border-gray-300 rounded-md"
-                  />
-                </div>
-              </div>
-            </>
-          )}
-        </div>
-
-        {/* Notes section removed */}
       </div>
     </div>
   );
